a1cadd62d2962fefce891281f7bcc50f360aba25
Roman Zolotorevich <roman@zolotorevich.com>
2024-01-26 17:43:49 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import argparse
+from datetime import datetime
+
+import connection
+from crawlers.abstract import Crawler
+from crawlers.factory import CrawlersFactory
+
+
+def main(crawlers_list: list[Crawler], dry_run: bool) -> None:
+
+    total: int = 0
+
+    # Launch crawlers and save data
+    for crawler in crawlers_list:
+        try:
+            # Collect data
+            crawler.collect()
+
+            # Check for dry run
+            if dry_run:
+                for item in crawler.payload:
+                    print(f'\n{item}')
+                continue
+
+            # Write to database
+            data = [item.__dict__ for item in crawler.payload]
+            rows = connection.write(crawler.table, data)
+            print(f'{crawler} +{rows}')
+
+            total += rows
+            
+        except (ConnectionError, AttributeError, TimeoutError) as error:
+            # TODO Request website or SQL error
+            print(f'[!] {crawler}: {error}')
+
+    print(f'\nTotal: +{total} @ {datetime.now().strftime("%H:%M")}')
+
+if __name__ == "__main__":
+
+    # Check CLI flags
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--crawler", dest="crawler", required=True,
+                        help="Category or crawler name")
+
+    parser.add_argument("-d", "--dry", dest="dry", action='store_true',
+                        help="Don't write to database")
+    
+    args = parser.parse_args()
+
+    # Get objects from crawlers factory
+    factory = CrawlersFactory()
+    crawlers_list = factory.register(args.crawler)
+
+    main(crawlers_list, args.dry)
---
+++ debug/debug.py
@@ -0,0 +1,30 @@
+"""Debug functions"""
+import os
+
+DEBUG_DIR = os.path.abspath(os.path.dirname(__file__))
+
+def dump_to_file(content,
+                 filename='dump.html',
+                 terminate=True):
+        
+    with open(f'{DEBUG_DIR}/{filename}', 'w') as file:
+        file.write(str(content))
+
+    if terminate:
+        print('Debug: terminate')
+        exit(0)
+
+def add_to_file(content, filename='flow.html'):
+    with open(f'{DEBUG_DIR}/{filename}', 'a') as file:
+        file.write(f'{str(content)}\n\n')
+
+"""IDEAS
++ timeprofile
++ dump object
++ check if debug left in files
++ decorator: return False, True, Other + run original def?
++ decorator: sleep(60)
++ decorator: change def input
++ 
+
+"""
